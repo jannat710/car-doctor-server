b4368939e79eacb9ae2f917174eae1680245ddb8
e <jannatul0040@gmail.com>
2023-10-28 23:30:37 +0600

    Load single service data

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const cors = require('cors');
 const app = express();
@@ -35,6 +35,20 @@ async function run() {
     app.get('/services', async (req, res) => {
         const cursor = serviceCollection.find();
         const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    //load single service data(59-3)
+    app.get('/services/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+
+        const options = {
+            // Include only the `title` and `imdb` fields in the returned document
+            projection: { title: 1, price: 1, service_id: 1, img: 1 },
+        };
+
+        const result = await serviceCollection.findOne(query, options);
         res.send(result);
     })
 
